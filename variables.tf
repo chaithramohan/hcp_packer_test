2b384315ebb92d8dd41a80a2b63e1483bd772957
chaithramohan <chaithrarejith@gmail.com>
2022-01-17 11:08:21 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,8 @@
+variable "project_id" {
+  type = string
+  description = "The project id of the GCP project"
+}
+variable "region" {
+  type = string
+  description = "The region of the GCP project"
+}
