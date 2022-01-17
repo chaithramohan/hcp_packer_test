8c18fdaf58949f11515454709ed1cf6b4c2764d9
chaithramohan <chaithrarejith@gmail.com>
2022-01-17 12:40:26 +0530

    Update autoscaler.tf

--- autoscaler.tf
+++ autoscaler.tf
@@ -44,7 +44,7 @@ resource "google_compute_instance_template" "foobar" {
     foo = "bar"
   }
   service_account {
-    email = ["deploymentautoscaler@internal-interview-candidates.iam.gserviceaccount.com"]
+    email = "deploymentautoscaler@internal-interview-candidates.iam.gserviceaccount.com"
     scopes = ["cloud-platform"]
   }
 
