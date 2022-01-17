6cb91bf1f571ad65371d77e80021dd3e84c14294
chaithramohan <chaithrarejith@gmail.com>
2022-01-17 11:18:37 +0530

    Create firewall.tf

---
+++ firewall.tf
@@ -0,0 +1,12 @@
+resource "google_compute_firewall" "rules" {
+  name        = "my-firewall-rule"
+  network     = "vpc-network"
+  description = "Creates firewall rule targeting tagged instances"
+
+  allow {
+    protocol  = "tcp"
+    ports     = ["80", "8080", "1000-2000"]
+  }
+
+  source_tags = ["0.0.0.0/0"]
+}
