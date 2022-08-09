68237b3457cd446d807e6c2ea6d8ca2228278586
chaithramohan <chaithrarejith@gmail.com>
2022-08-09 19:09:21 +0530

    Delete install_apache.sh

--- install_apache.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-sudo apt-get update && apt-get -y install apache2
-echo '<!doctype-html><html><body><h1> Hi!! Apache is Running here ! </h1></body></html>' | sudo tee /var/www/html/index.html
