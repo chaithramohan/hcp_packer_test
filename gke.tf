6dd6288dad4b4a50790d890dcafe2f5e49c1d6f5
chaithramohan <chaithrarejith@gmail.com>
2022-08-10 10:54:11 +0530

    Update gke.tf

--- gke.tf
+++ gke.tf
@@ -21,3 +21,20 @@ resource "google_gke_hub_membership" "membership" {
   provider = google-beta
   project  = var.project_id
 }*/
+resource "google_gke_hub_feature_membership" "feature_member" {
+  location = "global"
+  feature = google_gke_hub_feature.feature.name
+  membership = google_gke_hub_membership.membership.membership_id
+  configmanagement {
+    version = "1.8.0"
+    config_sync {
+      git {
+       sync_repo = “https://github.com/terraform-google-modules/terraform-google-kubernetes-engine.git”
+        sync_branch = “master”
+   policy_dir = “examples/acm-terraform-blog-part1/config-root”
+        secret_type = "none"
+      }
+    }
+  }
+  provider = google-beta
+}
