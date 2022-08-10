e1da270b623fbf7a2ddc1d0a789ad365c283066a
chaithramohan <chaithrarejith@gmail.com>
2022-08-10 10:18:56 +0530

    Update gke.tf

--- gke.tf
+++ gke.tf
@@ -1,7 +1,7 @@
 resource "google_container_cluster" "cluster" {
   name               = "my-cluster"
   location           = "us-west1-a"
-  project_id         = var.project_id
+  project_id         = "burner-chamohan3"
   initial_node_count = 1
   provider = google-beta
 }
