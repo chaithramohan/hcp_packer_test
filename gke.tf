f81fa524a2a20fe96370d7665582249d0796f073
chaithramohan <chaithrarejith@gmail.com>
2022-08-10 10:55:10 +0530

    Update gke.tf

--- gke.tf
+++ gke.tf
@@ -29,9 +29,9 @@ resource "google_gke_hub_feature_membership" "feature_member" {
     version = "1.8.0"
     config_sync {
       git {
-       sync_repo = “https://github.com/terraform-google-modules/terraform-google-kubernetes-engine.git”
+       sync_repo = "https://github.com/terraform-google-modules/terraform-google-kubernetes-engine.git"
         sync_branch = “master”
-   policy_dir = “examples/acm-terraform-blog-part1/config-root”
+   policy_dir = "examples/acm-terraform-blog-part1/config-root"
         secret_type = "none"
       }
     }
